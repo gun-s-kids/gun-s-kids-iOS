fa210c1ba489ff6c8b7f9042c323281df09a6f29
권준상 <wnstkdsun97@naver.com>
2024-03-03 15:39:59 +0900

    feat : 동아리 추가 버튼

--- gun-s-kids-iOS/Feature/ClubList/ClubListView.swift
+++ gun-s-kids-iOS/Feature/ClubList/ClubListView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ClubListView: View {
     @StateObject var viewModel: ClubListViewModel
+    @State var isButtonPressed = false
     
     init(){
         self._viewModel = StateObject.init(wrappedValue: ClubListViewModel())
@@ -16,16 +17,20 @@ struct ClubListView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView(.vertical) {
-                Spacer(minLength: 30)
-                CompanySectionView(viewModel: viewModel)
-                Spacer(minLength: 30)
-                ClubSectionView(viewModel: viewModel)
-                Spacer(minLength: 10)
-            }.toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    navigationBarButtonStack
+            ZStack(alignment: .bottomTrailing) {
+                ScrollView(.vertical) {
+                    Spacer(minLength: 30)
+                    CompanySectionView(viewModel: viewModel)
+                    Spacer(minLength: 30)
+                    ClubSectionView(viewModel: viewModel)
+                    Spacer(minLength: 10)
+                }.toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        navigationBarButtonStack
+                    }
                 }
+                registerButton
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 20))
             }
         }
     }
@@ -44,6 +49,24 @@ extension ClubListView {
                     .resizable()
                     .frame(width: 30, height: 30)
             }
+        }
+    }
+    
+    var registerButton: some View {
+        NavigationLink(destination: MakeClubView(), isActive: $isButtonPressed) {
+            Button(action: {
+                isButtonPressed = true
+            }) {
+                HStack {
+                    Image(systemName: "plus")
+                    Text("동아리")
+                        .font(.title3)
+                }
+            }
+            .padding()
+            .background(Color.mainColor4)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
         }
     }
 }
---
+++ gun-s-kids-iOS/Feature/ClubList/MakeClubView.swift
@@ -0,0 +1,20 @@
+//
+//  MakeClubView.swift
+//  gun-s-kids-iOS
+//
+//  Created by 권준상 on 2024/03/03.
+//
+
+import SwiftUI
+
+struct MakeClubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MakeClubView_Previews: PreviewProvider {
+    static var previews: some View {
+        MakeClubView()
+    }
+}
