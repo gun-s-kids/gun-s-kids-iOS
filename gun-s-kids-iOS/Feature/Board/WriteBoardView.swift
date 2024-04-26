db15e4f73ffca82e7f682c07567c12af99e6a06a
권준상 <wnstkdsun97@naver.com>
2024-04-26 16:58:27 +0900

    feat : 사진첩 버튼 추가

--- gun-s-kids-iOS/Feature/Board/WriteBoardView.swift
+++ gun-s-kids-iOS/Feature/Board/WriteBoardView.swift
@@ -9,42 +9,56 @@ import SwiftUI
 
 struct WriteBoardView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State var showImagePicker = false
     @State var title: String = ""
     @State var content: String = ""
     @State var placeholderText: String = "내용을 입력해주세요."
+    @State var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var isExistedImage: Bool = false
+    
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        postImage = Image(uiImage: selectedImage)
+        isExistedImage = true
+    }
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 15) {
-                Spacer(minLength: 15)
-                TextField("", text: $title,
-                          prompt: Text("제목을 입력해주세요.").foregroundColor(Color.grayColor))
-                    .font(.system(size: 24, weight: .semibold))
-                    .foregroundColor(.black)
-                    .padding(.leading, 3)
-                
-                ZStack(alignment: .topLeading) {
-                    if self.content.isEmpty {
-                        TextEditor(text: $placeholderText)
-                            .font(.system(size: 20))
-                            .foregroundColor(Color.grayColor)
-                            .disabled(true)
-                    }
-                    TextEditor(text: $content)
-                        .font(.system(size: 20))
+            ZStack(alignment: .bottomTrailing) {
+                VStack(alignment: .leading, spacing: 15) {
+                    Spacer(minLength: 15)
+                    TextField("", text: $title,
+                              prompt: Text("제목을 입력해주세요.").foregroundColor(Color.grayColor))
+                        .font(.system(size: 24, weight: .semibold))
                         .foregroundColor(.black)
-                        .opacity(self.content.isEmpty ? 0.25 : 1)
+                        .padding(.leading, 3)
+                    
+                    ZStack(alignment: .topLeading) {
+                        if self.content.isEmpty {
+                            TextEditor(text: $placeholderText)
+                                .font(.system(size: 20))
+                                .foregroundColor(Color.grayColor)
+                                .disabled(true)
+                        }
+                        TextEditor(text: $content)
+                            .font(.system(size: 20))
+                            .foregroundColor(.black)
+                            .opacity(self.content.isEmpty ? 0.25 : 1)
+                    }
                 }
-            }
-            .padding(.horizontal, 20)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    backButton
+                .padding(.horizontal, 20)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        backButton
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        enrollButton
+                    }
                 }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    enrollButton
-                }
+                addImageButton
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 20))
             }
         }
         .navigationBarBackButtonHidden(true)
@@ -69,6 +83,27 @@ extension WriteBoardView {
             Image(systemName: "plus")
                 .aspectRatio(contentMode: .fit)
                 .tint(.black)
+        }
+    }
+    
+    var addImageButton: some View {
+        Button(action: {
+            showImagePicker.toggle()
+        }) {
+            HStack {
+                Image(systemName: "photo.fill")
+                Text("사진첩")
+                    .font(.title3)
+            }
+        }
+        .padding()
+        .background(Color.mainColor4)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .sheet(isPresented: $showImagePicker, onDismiss: {
+            loadImage()
+        }) {
+            ImagePicker(image: $selectedImage)
         }
     }
 }
